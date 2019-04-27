1485bc022a18b56758bad4e3f95c215c04e781a3
Andi Ersaldy Raisha <andi.ersaldy@gmail.com>
2019-04-27 18:05:51 +0700

    Test cases added in readme

--- build/js/main.min.js
+++ build/js/main.min.js
@@ -3,44 +3,59 @@
   angular.module('fraction-calc', [])
   
   .controller('calcCtrl', ['$scope', ($scope) => {
-    $scope.cleanInput = null
     $scope.fractions = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]
     $scope.countedFractions = {}
+    $scope.input = ''
 
-    $scope.calculateFractions = () => {
-      $scope.countedFractions = {}
-      if ($scope.validateInput()) {
+    $scope.calculateFractions = (input) => {
+      const cleanInput = $scope.validateInput(input)
+      
+      if (cleanInput) {
+        let remainder = cleanInput
+        const countedFractions = {}
         const sortedFractions = $scope.fractions.sort((a, b) => {
           return (b - a)
         })
-        
-        let remainder = $scope.cleanInput
   
         sortedFractions.forEach((fraction) => {
           if (remainder >= fraction) {
-            $scope.countedFractions[fraction] = Math.floor(remainder / fraction)
+            countedFractions[fraction] = Math.floor(remainder / fraction)
             remainder = remainder % fraction
           }
         })
   
-        if (remainder > 0) 
-          $scope.countedFractions.remainder = remainder      
+        if (remainder > 0) { 
+          countedFractions.remainder = remainder
+        }
+
+        return countedFractions
+
       } else {
-        alert('Invalid input')
+        return false
       }
     }
 
-    $scope.validateInput = () => {
+    $scope.validateInput = (input) => {
       const regex = /^(Rp|Rp )*(0)*([1-9]\d{0,2})((\.\d{3})*|([0-9]*))(,00)*$/
 
-      if (regex.test($scope.input)) {
-        $scope.cleanInput = $scope.input.split(',')[0].replace(/\D/g, '')
-        return true
+      if (regex.test(input)) {
+        return input.split(',')[0].replace(/\D/g, '')
       }
       return false
     }
 
-  }])
+    $scope.eventHandler = () => {
+      const countedFractions = $scope.calculateFractions($scope.input)
+      
+      if (countedFractions) {
+        $scope.countedFractions = Object.assign(countedFractions)
+        console.log(countedFractions)
+      } else {
+        $scope.countedFractions = {}
+        alert('Invalid input')
+      }
+    }
 
+  }])
 
 })()
